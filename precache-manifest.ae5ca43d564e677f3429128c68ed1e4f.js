03e42049020ef6df976dd4916ecd80a2df7e1bac
Ashwani Singal <singalashwani@rediffmail.com>
2020-08-01 13:36:45 +0530

    Updates

---
+++ precache-manifest.ae5ca43d564e677f3429128c68ed1e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455bcb45eaaa29e82de94e4188de349d",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "30579cbec9d039af339a",
+    "url": "/contacts/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "456a90ab1bc43597ce7e",
+    "url": "/contacts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30579cbec9d039af339a",
+    "url": "/contacts/static/js/2.acaf16e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts/static/js/2.acaf16e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456a90ab1bc43597ce7e",
+    "url": "/contacts/static/js/main.036bc9e8.chunk.js"
+  },
+  {
+    "revision": "cc30bc0c76a2ddc6124d",
+    "url": "/contacts/static/js/runtime-main.dc4d3d8e.js"
+  }
+]);
